d0f1cdb745e5c18cbf24b41ce51f024dd748cdd1
Kyungtae Kim <31517616+kt0755@users.noreply.github.com>
2019-04-12 14:31:09 -0400

    Update sample.py

--- sample.py
+++ sample.py
@@ -2,6 +2,15 @@ import tensorflow as tf
 
 print(tf.__version__)
 
-hell = tf.constant("hello world")
-sess = tf.Session()
-print(sess.run(hello))
+
+### task 1
+#hello = tf.constant("hello world")  # making a node
+#sess = tf.Session()  # why need a session?? 
+#print(sess.run(hello))
+
+### task 2
+node1 = tf.constant(3.0, tf.float32)
+node2 = tf.constant(4.0)
+node3 = tf.add(node1, node2)
+print("node1:", node1, "node2:", node2)
+print("node3:", node3)
